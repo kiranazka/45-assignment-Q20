36ecb7d252e2433609816ec57c39e902162e0fa6
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-05-16 14:40:42 -0700

    45 Assignment
    Q20

---
+++ main.js
@@ -0,0 +1,6 @@
+// Making a PlaceName Array languages
+var placeName = ["Mount Everest", "k2", "Karachi", "Lahore", "Indus River", "Jhelum River", "Ravi River", "English", "urdu"];
+// Print the Message
+console.log("List of PlaceName:");
+// print the value of Array in the form of list 
+placeName.forEach(function (Name) { return console.log(Name); });
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Making a PlaceName Array languages
+let placeName: string[] = ["Mount Everest","k2","Karachi","Lahore","Indus River","Jhelum River","Ravi River","English","urdu"];
+// Print the Message
+console.log("List of PlaceName:");
+// print the value of Array in the form of list 
+placeName.forEach(Name => console.log(Name));
+
+
